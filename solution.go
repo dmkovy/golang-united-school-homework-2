55e7fb107c1d79a82798c2c7bf99a0073e1a82fc
Dm <dmkhost@gmail.com>
2022-05-06 13:04:11 +0200

    finally try

--- solution.go
+++ solution.go
@@ -1,21 +1,42 @@
 package square
 
-import "math"
+import (
+	"math"
+)
 
-type mySidesNumber int
+// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+type myCalcSquareInt int
 
-const SidesTriangle mySidesNumber = 3
-const SidesSquare mySidesNumber = 4
-const SidesCircle mySidesNumber = 0
+// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
+const (
+	SidesCircle   myCalcSquareInt = 0
+	SidesTriangle myCalcSquareInt = 3
+	SidesSquare   myCalcSquareInt = 4
+)
 
-func CalcSquare(sideLen float64, sidesNum mySidesNumber) float64 {
+// it's like:
+// CalcSquare(10.0, SidesTriangle)
+// CalcSquare(10.0, SidesSquare)
+// CalcSquare(10.0, SidesCircle)
+
+func CalcSquare(sideLen float64, sidesNum myCalcSquareInt) float64 {
+	var res float64
 	switch sidesNum {
-	case SidesTriangle:
-		return math.Sqrt(3) * sideLen * sideLen / 4
-	case SidesSquare:
-		return sideLen * sideLen
 	case SidesCircle:
-		return math.Pi * sideLen * sideLen
+		res = sideLen * sideLen * math.Pi // count sideLen as radius
+	case SidesTriangle:
+		res = sideLen * sideLen * math.Sqrt(3) / 4
+	case SidesSquare:
+		res = sideLen * sideLen
+	default:
+		res = 0
 	}
-	return 0
+	return res
 }
+
+//func main() {
+//	fmt.Println(CalcSquare(10.0, SidesTriangle))
+//	fmt.Println(CalcSquare(10.0, SidesSquare))
+//	fmt.Println(CalcSquare(10.0, SidesCircle))
+//	fmt.Println(CalcSquare(10.0, 2))
+//}
